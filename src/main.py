bfa0af0207dcb998d2987ed9501554dcd4c8aff3
Enzo Lazzarini <enzolazzamor@gmail.com>
2025-07-12 16:29:18 -0300

    Changed main to parse args; Changed print status to inside build_table(); Tried to fix rainbow table, still not working

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
 from rainbow_table import RainbowTable
+import argparse
 
 if __name__ == "__main__":
-    rt = RainbowTable()
-
-    password = rt.check_password(
-        "1e65cf1485fa6b43f090a448feb1cd8931378e4c96daf245a6d96c264e55579b59ca80519d020cb394b7e501c71386d8aeaf503206de439c9d92558c8884812d"
+    parser = argparse.ArgumentParser(
+        description="A RainbowTable built in python for a bonus college assignment."
     )
 
-    if password:
-        print("Achou senha: ", password)
-    else:
-        print("Nao achou!")
+    parser.add_argument("--rows", type=int, help="Table row size.", default=12000)
+    parser.add_argument(
+        "--steps",
+        type=int,
+        help="How many steps (cols) the table should have.",
+        default=1000,
+    )
+
+    args = parser.parse_args()
+
+    rt = RainbowTable(rows=args.rows, steps=args.steps)
+
+    hashed_password = "1e65cf1485fa6b43f090a448feb1cd8931378e4c96daf245a6d96c264e55579b59ca80519d020cb394b7e501c71386d8aeaf503206de439c9d92558c8884812d"
+    while hashed_password:
+        password = rt.check_password(hashed_password)
+
+        if password:
+            print("Achou senha: ", password)
+        else:
+            print("Nao achou!")
+        print("\n----\n")
+
+        hashed_password = str(input("Digite o hash da senha: "))
--- src/rainbow_table.py
+++ src/rainbow_table.py
@@ -4,20 +4,16 @@ import random
 import time
 
 MIN_LENGTH = 5
-ALPHABET_LENGTH = 70
+ALPHABET_LENGTH = 62
+SPECIAL_CHARS = "!@#%&*()"
 
 
 class RainbowTable:
     def __init__(self, rows=8000, steps=600):
         self.rows = rows
         self.steps = steps
-        self.alphabet = list(string.ascii_letters + string.digits + "!@#%&*()")
-
-        print("===== BUILDING TABLE... =====")
-        start_time = time.perf_counter()
+        self.alphabet = list(string.ascii_letters + string.digits)
         self.table = self.__build_table()
-        end_time = time.perf_counter()
-        print(f"===== TABLE BUILT ===== (Time: {end_time - start_time:.2f}s)")
 
     def __random_password(self):
         password = ""
@@ -32,60 +28,72 @@ class RainbowTable:
 
         return password
 
-    def __hash(self, plaintext):
+    def __sha512_hash(self, plaintext):
         return hashlib.sha512(plaintext.encode()).hexdigest()
 
     def __reduce(self, hash, step):
-        data = hash + str(step)
-        rng = random.Random(data)
-        reduced = ""
+        seed = hashlib.sha512((hash + str(step)).encode()).hexdigest()
+        rng = random.Random(seed)
 
+        reduced = ""
         while True:
             stop_probability = rng.uniform(0, 1)
-            i = len(reduced)
-
-            if i >= MIN_LENGTH and stop_probability < 0.125:
+            if len(reduced) >= MIN_LENGTH and stop_probability < 0.125:
                 break
 
-            while len(data) < (i * 4 + 4):
-                data += hash
-
-            idx = (int(data[i * 4 : i * 4 + 4], 16) + step) % ALPHABET_LENGTH
+            idx = rng.randint(0, ALPHABET_LENGTH - 1)
             reduced += self.alphabet[idx]
 
         return reduced
 
     def __build_table(self):
         table = []
-        for _ in range(self.rows):
+        print("==> BUILDING TABLE...")
+        start_time = time.perf_counter()
+        for row in range(self.rows):
+            print(f"\rBuilding row: {row+1}/{self.rows}", end="", flush=True)
             start = self.__random_password()
 
             end = start
             for step in range(self.steps):
-                h = self.__hash(end)
+                h = self.__sha512_hash(end)
                 end = self.__reduce(h, step)
 
             table.append((start, end))
+        end_time = time.perf_counter()
+        print(f"\r==> TABLE BUILT{" "* 20}", flush=True)
+        print(f"==> Time: {end_time - start_time:.2f}s")
+        print(f"==> Random row: { table[random.randint(0, self.rows) ]}")
 
         return table
 
+    def __regenerate(self, start, target_hash):
+        password = start
+        for step in range(self.steps):
+            if self.__sha512_hash(password) == target_hash:
+                return password
+            password = self.__reduce(self.__sha512_hash(password), step)
+        return None
+
     def check_password(self, hashed_password):
-        def find_password(row):
-            start_password = self.table[row][0]
-
-            for step in range(self.steps):
-                current_hash = self.__hash(start_password)
-
-                if hashed_password == current_hash:
-                    return start_password
-
-                start_password = self.__reduce(current_hash, step)
-            return None
-
+        max = 0
         for step in range(self.steps - 1, -1, -1):
-            reduced_hash = self.__reduce(hashed_password, step)
-            
-            for row in range(self.rows):
-                if reduced_hash == self.table[row][1]:
-                    return find_password(row)
+            candidate = hashed_password
+
+            for k in range(step, self.steps):
+                candidate = self.__reduce(self.__sha512_hash(candidate), k)
+                if len(candidate) > max:
+                    max = len(candidate)
+
+                print(
+                    f"\r==> {k}:{candidate}{" " * (max-len(candidate))}",
+                    end="",
+                    flush=True,
+                )
+
+            for start, end in self.table:
+                if candidate == end:
+                    return self.__regenerate(start, hashed_password)
+
+        print()
         return None
